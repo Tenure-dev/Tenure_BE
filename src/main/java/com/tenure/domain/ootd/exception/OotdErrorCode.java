a55303516902b7f31d2e3dcbb2154034c27272df
soseongmin03 <sosungmin0122@gmail.com>
2026-07-14 22:34:30 +0900

    feature(feed): 전체 피드 조회 추가

--- src/main/java/com/tenure/domain/ootd/exception/OotdErrorCode.java
+++ src/main/java/com/tenure/domain/ootd/exception/OotdErrorCode.java
@@ -8,9 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum OotdErrorCode implements ErrorCode {
-    OOTD_IMAGE_REQUIRED("OOTD_001", "OOTD 이미지가 필요합니다.", HttpStatus.BAD_REQUEST),
-    OOTD_SOURCE_INVALID("OOTD_002", "서비스 내 카메라로 촬영한 사진만 게시할 수 있습니다.", HttpStatus.BAD_REQUEST),
-    OOTD_NOT_FOUND("OOTD_404", "OOTD 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    OOTD_IMAGE_REQUIRED("OOTD_001", "대표 이미지는 필수입니다.", HttpStatus.BAD_REQUEST),
+    OOTD_SOURCE_INVALID("OOTD_002", "앱 내 카메라로 촬영한 사진만 게시할 수 있습니다.", HttpStatus.BAD_REQUEST);
 
     private final String code;
     private final String message;
