8424ac573e2e88fd72234c8cd8d48b62350a0aee
Chungs0604 <alsk3528@gmail.com>
2026-07-20 17:19:30 +0900

    ✨ Feat: 채팅 내역 조회

--- src/main/java/com/tenure/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/tenure/domain/chat/repository/ChatMessageRepository.java
@@ -1,10 +1,13 @@
 package com.tenure.domain.chat.repository;
 
 import com.tenure.domain.chat.entity.ChatMessage;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
@@ -13,4 +16,16 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
             "where cm.chatRoom.id = :chatRoomId " +
             "order by cm.createdAt desc limit 1")
     Optional<ChatMessage> findByRecentMessage(@Param("chatRoomId") Long chatRoomId);
+
+
+    // 채팅 내역 조회
+    @Query("select cm from ChatMessage cm join fetch cm.sender " +
+            "where cm.chatRoom.id = :chatRoomId " +
+            "and (cm.createdAt < :cursor or (cm.createdAt = :cursor and cm.id < :cursorId)) " +
+            "order by cm.createdAt desc , cm.id desc ")
+    Slice<ChatMessage> findByChatMessages(@Param("chatRoomId") Long chatRoomId,
+                                          @Param("cursor") LocalDateTime cursor,
+                                          @Param("cursorId") Long cursorId,
+                                          Pageable pageable
+    );
 }
--- src/main/java/com/tenure/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/tenure/domain/chat/repository/ChatRoomRepository.java
@@ -9,4 +9,5 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
     //아이템, 판매자, 구매자로 채팅방 조회
     Optional<ChatRoom> findByItemIdAndSellerIdAndBuyerId(Long itemId, Long sellerId, Long buyerId);
+
 }
